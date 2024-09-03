6dc0b5d5ae38bd1768075fe6c74cb43d9618882c
jiyoungnim <cloundyon31@gmail.com>
2024-09-03 21:43:45 +0900

    9월 첫째주 두번째 커밋(+ 1)

--- PGS-Lv1-solved/Sep-challenge/09-second.js
+++ PGS-Lv1-solved/Sep-challenge/09-second.js
@@ -127,3 +127,56 @@ movements.sort((a, b) => {
 movements.sort((a, b) => b - a);
 // 📌 a > b (- : 아무 일도 안 일어나 a, b 그대로 => 결국 내림차순)
 // 📌 b > a (+ : 둘의 자리가 바뀌어 b, a로 바뀜 => 결국 내림차순)
+
+// 🛎️ 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다. 전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+function solution(phone_number) {
+  var answer = "";
+  for (let i = 0; i < phone_number.length; i++) {
+    if (i < phone_number.length - 4) {
+      answer += "*";
+    } else {
+      answer += phone_number[i];
+    }
+  }
+
+  return answer;
+}
+
+// 😲 다른 사람의 풀이
+// ✅ repeat method & slice method
+function hide_numbers(s) {
+  var result = "*".repeat(s.length - 4) + s.slice(-4);
+  return result;
+}
+
+// 💡 repeat method
+// EXAMPLE 1)
+const mood = "Happy! ";
+
+console.log(`I feel ${mood.repeat(3)}`);
+// Expected output: "I feel Happy! Happy! Happy! "
+
+// EXAMPLE 2)
+"abc".repeat(0); // ''
+"abc".repeat(1); // 'abc'
+"abc".repeat(2); // 'abcabc'
+"abc".repeat(3.5); // 'abcabcabc' (count will be converted to integer)
+
+// 💡 slice method
+console.log(animals.slice(2));
+// Expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animals.slice(2, 4));
+// Expected output: Array ["camel", "duck"]
+
+console.log(animals.slice(1, 5));
+// Expected output: Array ["bison", "camel", "duck", "elephant"]
+
+console.log(animals.slice(-2));
+// Expected output: Array ["duck", "elephant"]
+
+console.log(animals.slice(2, -1));
+// Expected output: Array ["camel", "duck"]
+
+console.log(animals.slice());
+// Expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
